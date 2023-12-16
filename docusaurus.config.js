9524851ee7cd0aaa07785a48af27c0f95d5d7511
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-12-15 19:07:42 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'PenguinMod', // Usually your GitHub org/user name.
   projectName: 'PenguinMod-DocsNewest', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
