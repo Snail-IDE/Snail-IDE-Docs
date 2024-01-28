59b2c8b6693d4df5c3f287e01d2c8c757068a3b8
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-01-27 19:49:17 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 const config = {
   title: 'Snail IDE Docs',
   // tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'https://www.snail-ide.com/favicon.ico',
 
   // Set the production url of your site here
   url: 'https://snail-ide-docs.vercel.app',
